8d0b0c7c6e2b35bc4af0167fd1b020f0e458b361
johnnatha.santos <johnnatha@fullstack-service.com.br>
2020-02-10 00:56:26 -0300

    Papafila fase 2 - Cupom de Desconto

---
+++ src/styles/dash-regionais.js
@@ -0,0 +1,105 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const buttonFilterDefault = theme => ({
+    boxShadow: 'none',
+    marginRight: 10,
+    height: 30,
+    lineHeight: '15px',
+    border:`1px solid ${theme.palette.primary.pill.background}`,
+    color: theme.palette.primary.text,
+    backgroundColor: 'transparent',
+})
+
+const dashRegioesStyles = makeStyles(theme => ({
+    root: {
+
+
+    },
+    wrapperItemName: {
+        display: 'flex',
+        alignItems: 'center',
+        color: theme.palette.primary.text,
+        borderRight:'1px solid white'
+    },
+    dataItem: {
+        borderBottom: '0.55px solid #eaeaea',
+        margin: '3px -4px',
+
+        '&:last-child': {
+            border: 'none'
+        }
+    },
+    smallFont: {
+      fontSize: 12,
+        color: theme.palette.primary.accentStrongText,
+    },
+    header: {
+        '& div': {
+            color: theme.palette.primary.text,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+        },
+        '& span': {
+            display: 'inline-block',
+            marginLeft: 5
+        }
+    },
+    spacerBottom: {
+        paddingBottom: 70
+    },
+    buttonFilter: {
+        ...buttonFilterDefault(theme)
+    },
+    buttonFilterActive: {
+        ...buttonFilterDefault(theme),
+        backgroundColor: theme.palette.primary.pill.background,
+        color: '#fff'
+    },
+    wrapperItem: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        color: theme.palette.primary.text
+    },
+    wrapperItemPill: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        fontSize: 12,
+        borderRight: '1px solid white'
+    },
+    wrapperItemPercentPill: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: '4px 28px !important',
+        fontSize: 12
+    },
+    wrapperItemIcon: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    successIcon: {
+        fontSize: 34,
+        color: '#4cac5b'
+    },
+    dangerIcon: {
+        fontSize: 34,
+        color: '#c24131'
+    },
+    '@media only screen and (max-width: 1200px)': {
+        rootMobile: {
+            width: '100%',
+            overflowX: 'scroll',
+
+            '& > div': {
+                width: 1250
+            }
+        }
+    }
+}));
+
+export default dashRegioesStyles
